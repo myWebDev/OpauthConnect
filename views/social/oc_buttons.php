c17638839b130e821320809374f3b11711db2557
Tristan van Bokkem <tristanvanbokkem@gmail.com>
2014-04-09 18:28:49 +0200

    Added h3 tag at login page

--- views/social/oc_buttons.php
+++ views/social/oc_buttons.php
@@ -1,17 +1,17 @@
 <?php
 /**
  * OpauthConnect
- * 
+ *
  * @copyright Copyright © 2012 Oleksandr Golubtsov
  * @license   GPLv2 License
  * @package   OpauthСonnect
- * 
+ *
  * This file is part of OpauthСonnect plugin. Please see the included license file for usage information
  */
 
 /**
  * Available variables:
- * 
+ *
  * $data["remember"] - Does plugin set login cookies
  * $data["services"] - Array of available services. Each service has following structure:
  *                     "service_name" => array(
@@ -20,7 +20,8 @@
  *                     )
  */
 ?>
-
+<div style="border-bottom: 1px solid #d5d5d5">&nbsp;</div>
+<h3><?php echo T("Or Log In Using")?></h3>
 <div id="social-container">
     <?php foreach($data['services'] as $service_name => $service): ?>
         <a href="<?php print $service['url']; ?>" class="button toggle">
@@ -29,7 +30,7 @@
         </a>
     <?php endforeach; ?>
 </div>
-    
+
 <div id="remember-container">
     <a href="#" class="button toggle <?php if($data["remember"]): ?>button-pressed<?php endif; ?>">
         <i class="icon-check<?php if(!$data["remember"]): ?>-empty<?php endif; ?>"></i> <?php print T('Keep me logged in'); ?>
